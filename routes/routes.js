afc2a828b1e5e03bf1f21fe4accaf3c2385c0eaf
LexieLei98 <Lexie.Lei@kpmg.co.uk>
2023-01-23 17:07:34 +0000

    get all farms

--- routes/routes.js
+++ routes/routes.js
@@ -22,8 +22,14 @@ router.post('/farms', async(req, res) => {
 })
 
 //Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/farms', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Get by ID Method
