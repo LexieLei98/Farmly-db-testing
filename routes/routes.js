4af7abe9a2425fd1259138b5bc05f9a605c9f316
reawilliams20 <Andrea.NgokaWilliams@kpmg.co.uk>
2023-01-23 17:03:18 +0000

    get farm

--- routes/routes.js
+++ routes/routes.js
@@ -27,8 +27,14 @@ router.get('/getAll', (req, res) => {
 })
 
 //Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/farms/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Update by ID Method
