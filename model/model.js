e54e87e4204cc2c11058ff85dc2bbfedd28bab2d
LexieLei98 <Lexie.Lei@kpmg.co.uk>
2023-01-23 13:58:34 +0000

    first commit

---
+++ model/model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+//creating a new folder in the database
+const farmSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    address: {
+        required: true,
+        type: Object
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    profile_pic: {
+        required: true,
+        type: String
+    },
+    rating: {
+        required: true,
+        type: Array
+    },
+    produce: {
+        required: true,
+        type: Array
+    },
+})
+
+module.exports = mongoose.model('Farms', farmSchema)
---
+++ routes/routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router()
+const Model = require('../model/model');
+
+//Post Method
+router.post('/post', async(req, res) => {
+    const data = new Model ({
+        name: req.body.name,
+        address: req.body.address,
+        description: req.body.description,
+        profile_pic: req.body.profile_pic,
+        rating: req.body.rating,
+        produce: req.body.produce
+    })
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+module.exports = router;
