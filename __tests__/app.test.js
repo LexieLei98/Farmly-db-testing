6044a43d9d1de58d856254d5e13670cd6f0a3627
CocoS26 <Coco.Shen@KPMG.co.uk>
2023-01-26 16:54:08 +0000

    finalised

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -159,17 +159,6 @@ beforeEach(async () => {
     await mongoose.connection.close();
   });
 
-  describe('GET /api for all non-existent routes in app', () => {
-    test('404: non existent path', () => {
-        return request(app)
-        .get('/not-a-route')
-        .expect(404)
-        .then((body) => {
-            console.log(body)
-        })
-    })
-});
-
   describe('POST /api/farms', () => {
     test('status:201 returns posted farm', () => {
         const newFarm = {
