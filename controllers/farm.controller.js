1b89b71b051d46eadc83096b16f72a40db539ab2
joshnguyen98 <josh.nguyen@kpmg.co.uk>
2023-01-31 11:05:32 +0000

    sorting by location

--- controllers/farm.controller.js
+++ controllers/farm.controller.js
@@ -23,7 +23,7 @@ const postFarm = async(req, res, next) => {
 //Get all Method
 const getFarms = async (req, res, next) => {
     try{
-        const data = await Farm.find();
+        const data = await Farm.find().sort( { "distance_from_location": 1, "_id": 1 } );
         res.json(data)
     }
     catch(error){
